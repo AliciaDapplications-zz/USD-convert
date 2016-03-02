e8abc56a607f4326671650824d17826c0d569dc6
Alicia Dale <boricua21dale@gmail.com>
2016-03-02 14:03:00 -0600

    Initial Commit

---
+++ USD convert/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  USD convert
+//
+//  Created by Alicia Dale on 3/2/16.
+//  Copyright © 2016 Alicia Dale. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
